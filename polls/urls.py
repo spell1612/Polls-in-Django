23f5d840b2bdec8a4753b37248c15c80ddf628ee
spell1612 <spell1612@gmail.com>
2018-08-28 17:00:34 +0530

    added proper views

--- polls/urls.py
+++ polls/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name='polls' #This is cause there might othher apps with same url name, so a common template should be able to recognize. This is called namespacing
 urlpatterns = [
     path('', views.firstfun,name='index'),
-    path('<int:question_id>',views.details), #question_id will have the question number, ie link will be polls/5 if its qno 5
-    path('<int:question_id>/results',views.results),
-    path('<int:question_id>/vote',views.vote),
+    path('<int:question_id>/',views.details,name='details'), #question_id will have the question number, ie link will be polls/5 if its qno 5
+    path('<int:question_id>/results/',views.results,name='results'),
+    path('<int:question_id>/vote/',views.vote,name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,43 @@
-from django.shortcuts import HttpResponse
-from .models import Question
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+
+from .models import Choice,Question
 
 # Create your views here.
 def firstfun(request):
     qdatelist=Question.objects.order_by('-pub_date')[:5] #wow a list from 0-4 (an array of objects) made from the return o a function call!
-    response=',\n'.join([pro.ques_text for pro in qdatelist] )  #notice this is a list inside the join function! the \n doesnt do anything, need to find replacement
-    return HttpResponse(response) #in the pro.ques_text the obj pro can be anything for some reason O_O python is weird
+    # response=',\n'.join([pro.ques_text for pro in qdatelist] )  #notice this is a list inside the join function! the \n doesnt do anything, need to find replacement
+    #in the pro.ques_text the obj "pro" can be anything for some reason. O_O python is weird
+    context={
+        'latestqlist':qdatelist,  #this is a dictionary
+        # 'debug':"nobugs"
+    }
+    return render(request,'polls/index.html',context) #render() for templates HttpResponse() for single line responses
 
 def details(request,question_id):
-    return HttpResponse("You're looking at question number %s" %question_id)
+    # try:
+    #     q=Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("The requested page does not exist")
+    q=get_object_or_404(Question,pk=question_id) #this works instead of commented code
+    return render(request, 'polls/details.html',{'question':q}) #inline context
+    # There’s also a get_list_or_404() function, which works just as get_object_or_404() – except using filter() instead of get(). It raises Http404 if the list is empty.
 
 def results(request, question_id):
-    response="You're looking at result of question %s"
-    return HttpResponse(response % question_id)
-    
+    q=get_object_or_404(Question,pk=question_id)
+    return render(request, 'polls/results.html',{'question':q})
+
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    q=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=q.choice_set.get(pk=request.POST['choice']) #request.POST['choice'] is same as using the name of the field in laravel (for comparison). Its value will be the choice.id that was clicked on
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/details.html',{'question':q,'error_message': "You didn't select a choice."}) # Redisplay the question voting form.
+    else:
+        selected_choice.votes +=1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(q.id,))) #notice that args takes a 1 tuple, ie (q.id, <empty>) not an integer
